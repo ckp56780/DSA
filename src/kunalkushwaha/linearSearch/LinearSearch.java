4efe8753316a6b0e3a57321820d26c47b0f06485
ckp56780 <ckp56780@gmail.com>
2024-09-29 23:24:34 +0530

    second commit

--- src/kunalkushwaha/linearSearch/LinearSearch.java
+++ src/kunalkushwaha/linearSearch/LinearSearch.java
@@ -1,2 +1,35 @@
-package kunalkushwaha.linearSearch;public class LinearSearch {
+package kunalkushwaha.linearSearch;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr={10,12,13,14,19,-4,15,20};
+        int target=20;
+
+        //calling the function
+        int ans=linearSearch(arr,target);
+        System.out.println(ans);
+
+    }
+    public static int linearSearch(int[] nums ,int target){
+        if(nums.length==0){
+            return -1;
+        }
+
+        for(int element:nums){
+            if(element==target){
+                return element;
+            }
+        }
+        return -1;
+//        for ( int index=0;index<nums.length;index++){
+//            int element=nums[index];  //here we will find the value at given index
+//            if(element==target){
+//                return nums[index];//index //element
+//            }
+//        }
+//        return -1;
+
+    }
+
+
 }
--- src/kunalkushwaha/linearSearch/LinearWithString.java
+++ src/kunalkushwaha/linearSearch/LinearWithString.java
@@ -1,2 +1,27 @@
-package kunalkushwaha.linearSearch;public class LinearwithString {
+package kunalkushwaha.linearSearch;
+
+public class LinearWithString {
+    public static void main(String[] args) {
+
+        String str="chandan";
+        char target='f';
+
+        //calling the function for finding the char "d" int the string
+
+        boolean ans=linearWithString(str,target);
+        System.out.println(ans);
+    }
+    public static boolean linearWithString(String str1, char target){
+        if(str1.length()==0){
+            return false;
+        }
+        for(int i=0;i<str1.length();i++){
+            char element=str1.charAt(i);
+            if(element==target){
+                return  true;
+            }
+        }
+        return false;
+
+    }
 }
