871478640ca7a48a9df1f9ac227ed7ff073e861c
Chandan Kumar <shwetachanndu1994@gmail.com>
2025-05-17 12:15:13 +0530

    ceiling of the given number

---
+++ src/chandanPractise/ArraysProblems/topquestionLeetcode/CeilingOfGivenNumber_1.java
@@ -0,0 +1,14 @@
+package chandanPractise.ArraysProblems.topquestionLeetcode;
+
+//trick-sorted array applies the binary search will apply-
+//ceiling is - smallest no in the array which is greater than or equal to target.
+//arr-{2,3,5,9,14,16,18}
+
+//target-14 ,ceiling is -14
+//target-15, ceiling -16
+//target-4,ceiling-5
+//target-9,v=ceiling-9
+public class CeilingOfGivenNumber_1 {
+
+
+}
